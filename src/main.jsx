967b860161ed58ba0fa090416c95e9038b97ac67
Giovanni Ramirez <jesussavedgr78@gmail.com>
2026-08-30 15:32:06 -0400

    adding reactrouterdom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// Pages
 import App from './App.jsx'
+import NotFound from './pages/notFound.jsx';
+
+// Routes
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
